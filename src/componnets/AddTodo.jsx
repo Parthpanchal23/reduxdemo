16a80257842b93709ebf7e41b336e2c4f196cc6a
Parth Panchal <parthpanchal.2023@gmail.com>
2024-08-12 12:27:36 +0530

    Todo implemented

--- src/componnets/AddTodo.jsx
+++ src/componnets/AddTodo.jsx
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
 const AddTodo = () => {
-  const [input, setInput] = useState("");
   const dispatch = useDispatch();
+  const [input, setInput] = useState("");
 
   const addTodoHandler = (e) => {
     e.preventDefault();
-    dispatch(addTodo(input));
-    setInput("");
+
+    if (e.target[0].value && e?.target[0]?.value?.trim() == "") {
+      alert("todo Title is required");
+    } else {
+      dispatch(addTodo(input));
+      setInput("");
+    }
   };
+
   return (
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
       <input
@@ -17,6 +23,7 @@ const AddTodo = () => {
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
         value={input}
+        required
         onChange={(e) => setInput(e.target.value)}
       />
       <button
--- src/componnets/Todo.jsx
+++ src/componnets/Todo.jsx
@@ -4,16 +4,20 @@ import { removeTodo } from "../features/todo/todoSlice";
 const Todo = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
+
   return (
-    <ul className="list-none  items-center">
+    <ul className="list-none ">
       {todos?.map((todo) => (
         <li
-          className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+          className="mt-4 transition ease-in-out delay-150 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
           key={todo.id}
         >
           <div className="text-white">{todo.text}</div>
           <button
-            onClick={() => dispatch(removeTodo(todo.id))}
+            onClick={() => {
+              dispatch(removeTodo(todo.id));
+              alert("Deleted Sucessfully");
+            }}
             className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
           >
             <svg
